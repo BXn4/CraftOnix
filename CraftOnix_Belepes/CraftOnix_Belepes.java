c23d4a2d0a0068f085868751b5f3fd1754597123
Bence <78733248+BXn4@users.noreply.github.com>
2023-02-21 00:59:30 +0100

    Update CraftOnix_Belepes.java

--- CraftOnix_Belepes/CraftOnix_Belepes.java
+++ CraftOnix_Belepes/CraftOnix_Belepes.java
@@ -5,33 +5,50 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.HashMap;
 import java.util.UUID;
-import  java.io.*;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
-public class CraftOnix_Belepes extends JavaPlugin implements CommandExecutor {
-
+public class CraftOnix_Belepes extends JavaPlugin implements CommandExecutor, Listener {
     private HashMap<UUID, String> terkep = new HashMap<>();
-
     public void onEnable() {
         getCommand("belep").setExecutor(this);
         getCommand("regisztral").setExecutor(this);
     }
-
     public boolean onCommand(CommandSender kuldo, Command parancs, String szoveg, String[] args) {
         if (parancs.getName().equalsIgnoreCase("regisztral")) {
-            Player jatekos = (Player) kuldo;
-            String bemenet = String.join(" ", args);
-            if (bemenet.length() > 6 && !bemenet.contains(" "))
-            {
-                terkep.put(jatekos.getUniqueId(), bemenet);
-                jatekos.sendMessage("Sikeres regisztráció!" + jatekos.getName());
+            try {
+                if (parancs.getName().equalsIgnoreCase("regisztral")) {
+                    Player jatekos = (Player) kuldo;
+                    String bemenet = String.join(" ", args);
+                    if (bemenet.length() > 6 && !bemenet.contains(" ")) {
+                        String kulcsString = "KULCS:)";
+                        SecretKey kulcs = new SecretKeySpec(kulcsString.getBytes(), "AES");
+                        Cipher cipher = Cipher.getInstance("AES");
+                        cipher.init(Cipher.ENCRYPT_MODE, kulcs);
+                        byte[] byteok = cipher.doFinal(bemenet.getBytes());
+                        String jelszo = Base64.getEncoder().encodeToString(byteok);
+                        terkep.put(jatekos.getUniqueId(), jelszo);
+                        jatekos.sendMessage(" ");
+                        jatekos.sendMessage("§8[§2>>§8] §7Sikeres regisztráció! A legközelebbi csatlakozásnál a megadott jelszóval tudsz belépni.§r");
+                    } else {
+                        jatekos.sendMessage(" ");
+                        jatekos.sendMessage("§8[§4>>§8] §7A jelszavadnak minimum §l6 §r§7karakternek kell lennie, illetve nem tartalmazhat szóközt!§r");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
             }
-            else{
-                jatekos.sendMessage("§8[§4>>§8] §7A jelszavadnak minimum §l6 §r§7karakternek kell lennie, illetve nem tartalmazhat szóközt!§r" + jatekos.getName());
-            }
+
         }
-        return  true;
+        return true;
     }
 }
